d99740b1d802a583f40a2286e8fc15a2273a781f
xpvqx <kjs02x@gmail.com>
2025-12-24 01:54:05 +0100

    +

--- PowerShell/profile.ps1
+++
@@ -1,4 +0,0 @@
-function Get-Theme_Override {
-    $themePath = "$HOME\Documents\PowerShell\Themes\gruvbox.omp.json"
-    & "$env:LOCALAPPDATA\Programs\oh-my-posh\bin\oh-my-posh.exe" init pwsh --config $themePath | Invoke-Expression
-}
