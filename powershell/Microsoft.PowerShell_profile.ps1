f61ca9b93f7d08770039e4d43c5aacac97a7792b
xpvqx <kjs02x@gmail.com>
2026-01-28 00:15:56 +0100

    debloat ps profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,128 @@
+# prompt
+$ENV:STARSHIP_CONFIG = "$HOME\Documents\starship\starship.toml"
+Invoke-Expression (&starship init powershell)
+
+# editor
+$EDITOR = 'nvim'
+
+# install with: "Install-Module Terminal-Icons -Repository PSGallery -Scope CurrentUser"
+Import-Module Terminal-Icons
+
+# alias
+Set-Alias vim nvim
+Set-Alias c clear
+Set-Alias ll Get-ChildItem
+Set-Alias la "Get-ChildItem -Force"
+Set-Alias grep Select-String
+Set-Alias cat Get-Content
+Set-Alias touch New-Item
+Set-Alias rm Remove-Item
+
+# shortcut for editing profile
+function Edit-Profile { nvim $PROFILE }
+Set-Alias ep Edit-Profile
+
+# navigation
+function hv {
+    Set-Location "C:\Users\karlj\Nextcloud\hv"
+}
+
+function nit {
+    Set-Location "C:\Users\karlj\Nextcloud\hv\Kurser"
+}
+
+function ntk {
+    Set-Location "C:\Users\karlj\Nextcloud\hv\Påbyggnadsår"
+}
+
+function docs {
+    $docs = if(([Environment]::GetFolderPath("MyDocuments"))) {([Environment]::GetFolderPath("MyDocuments"))} else {$HOME + "\Documents"}
+    Set-Location -Path $docs
+}
+
+
+# sys and utils
+function winutil {
+  irm https://christitus.com/win | iex
+}
+
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+function df {
+    get-volume
+}
+
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function export($name, $value) {
+    set-item -force -path "env:$name" -value $value;
+}
+
+function head {
+    param($Path, $n = 10)
+    Get-Content $Path -Head $n
+}
+
+function tail {
+    param($Path, $n = 10, [switch]$f = $false)
+    Get-Content $Path -Tail $n -Wait:$f
+}
+
+# sudo = admin
+function admin { Start-Process wt -Verb runAs}
+Set-Alias sudo admin
+
+# Set-PSReadLineOption compatibility for powershell desktop
+function Set-PSReadLineOptionsCompat {
+    param([hashtable]$Options)
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        Set-PSReadLineOption @Options
+    } else {
+        # remove unsupported keys for Desktop and silence errors
+        $SafeOptions = $Options.Clone()
+        $SafeOptions.Remove('PredictionSource')
+        $SafeOptions.Remove('PredictionViewStyle')
+        Set-PSReadLineOption @SafeOptions
+    }
+}
+
+# enhancements
+$PSReadLineOptions = @{
+    EditMode = 'Windows'
+    HistoryNoDuplicates = $true
+    HistorySearchCursorMovesToEnd = $true
+    Colors = @{
+        Command = '#87CEEB'  # SkyBlue (pastel)
+        Parameter = '#98FB98'  # PaleGreen (pastel)
+        Operator = '#FFB6C1'  # LightPink (pastel)
+        Variable = '#DDA0DD'  # Plum (pastel)
+        String = '#FFDAB9'  # PeachPuff (pastel)
+        Number = '#B0E0E6'  # PowderBlue (pastel)
+        Type = '#F0E68C'  # Khaki (pastel)
+        Comment = '#D3D3D3'  # LightGray (pastel)
+        Keyword = '#8367c7'  # Violet (pastel)
+        Error = '#FF6347'  # Tomato (keeping it close to red for visibility)
+    }
+    PredictionSource = 'History'
+    PredictionViewStyle = 'ListView'
+    BellStyle = 'None'
+}
+Set-PSReadLineOptionsCompat -Options $PSReadLineOptions
+
+# custom key handlers
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardDeleteWord
+Set-PSReadLineKeyHandler -Chord 'Alt+d' -Function DeleteWord
+Set-PSReadLineKeyHandler -Chord 'Ctrl+LeftArrow' -Function BackwardWord
+Set-PSReadLineKeyHandler -Chord 'Ctrl+RightArrow' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Ctrl+z' -Function Undo
+Set-PSReadLineKeyHandler -Chord 'Ctrl+y' -Function Redo
